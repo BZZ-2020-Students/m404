147b4cb5eede3b5c9e9d7fe84fc0fb62375a7c22
Muetzilla <muetz-github@gasbichler.ch>
2021-05-18 15:10:38 +0200

    mit INTEGER

--- muetzilla_stuff/src/zahlenraten/GuessNumber.java
+++ muetzilla_stuff/src/zahlenraten/GuessNumber.java
@@ -42,40 +42,40 @@ public class GuessNumber {
         Integer max = new Integer(100);
         Integer min = new Integer(1);
         System.out.print("Hallo, versuchen Sie die Zahl (" + min + "-" + max + ") zu erraten!\nGeben Sie Ihren ersten Versuch ein > ");
-        int input = scanner.nextInt();
+        Integer input = scanner.nextInt();
         scanner.nextLine();
-        while (input > 100 || input < 1) {
+        while (input.compareTo(max) > 0 || input.compareTo(min) < 0) {
             System.out.println("Bitte eine gültige Zahl eingeben > ");
             input = scanner.nextInt();
             scanner.nextLine();
         }
         do {
-            if (input < secret) {
+            if (input.compareTo(secret) < 0) {
                 min = input;
                 System.out.println("Die gesuchte Zahl ist grösser als " + input + " Eingabebereich : " + min + "-" + max);
                 System.out.print("Versuchen Sie es erneut > ");
                 input = scanner.nextInt();
                 scanner.nextLine();
-                while (input > max || input < min) {
+                while (input.compareTo(max) > 0 || input.compareTo(min) < 0) {
                     System.out.print("Ihre Eingabe ist nicht im Zahlenbereich. Bitte versuchen Sie es erneut > ");
                     input = scanner.nextInt();
                     scanner.nextLine();
                 }
 
-            } else if (input > secret) {
+            } else if (input.compareTo(secret) > 0) {
                 max = input;
                 System.out.println("Die gesuchte Zahl ist kleiner als " + input + " Eingabebereich : " + min + "-" + max);
                 System.out.print("Versuchen Sie es erneut > ");
                 input = scanner.nextInt();
                 scanner.nextLine();
-                while (input > max || input < min) {
+                while (input.compareTo(max) > 0 || input.compareTo(min) < 0) {
                     System.out.print("Ihre Eingabe ist nicht im Zahlenbereich. Bitte versuchen Sie es erneut > ");
                     input = scanner.nextInt();
                     scanner.nextLine();
                 }
             }
             trys++;
-        } while (input != secret);
+        } while (!input.equals(secret));
         System.out.println("Gratulation! Sie haben die Zahl erraten. Sie haben dafür " + trys + " Versuche benötigt!");
     }
 }

dfa587fcf3d052037e273b2256e184ff281110bd
Phillip <jerebicp@bzz.ch>
2021-05-25 16:58:34 +0200

    update

--- phills_stuff/M404/src/notentool/Main.java
+++ phills_stuff/M404/src/notentool/Main.java
@@ -22,26 +22,35 @@ public class Main {
         };
 
         Fach[] faecher = {
-                new Fach(noten,"Englisch",2,2005),
-                new Fach(notenEn,"Deutsch",2,2005)
+                new Fach(noten,"Deutsch",2,2005),
+                new Fach(notenEn,"Englisch",2,2005)
+        };
+
+        Note[] notens2 = {
+                new Note(5.7,new Date(),0.5),
+                new Note(2.8,new Date(),0.5),
+                new Note(6,new Date(),1)
+        };
+        Note[] notenEns2 = {
+                new Note(5.8,new Date(),1),
+                new Note(4,new Date(),0.5),
+                new Note(3.75,new Date(),1)
+        };
+
+        Fach[] faechers2 = {
+                new Fach(notens2,"Deutsch",2,2005),
+                new Fach(notenEns2,"Englisch",2,2005)
         };
 
         Schueler s1 = new Schueler(faecher,"Mustermann","Sebastian","01.02.2007","Männlich");
+        Schueler s2 = new Schueler(faechers2,"Mustermann","Max","01.02.2007","Männlich");
 
         System.out.println("Nachname: " + s1.getName() + "\nVorname: " + s1.getVorname() + "\nGeburtstag: " + s1.getGeburtstag());
         s1.printFaecher();
         System.out.println("------------------------------------------------------------------");
-//        for (int i = 0; i < faecher.length; i++) {
-//            System.out.println(faecher[i].getName() + "\n" + faecher[i].printNoten());
-//            for (int y = 0; y < noten.length; y++) {
-//                System.out.println("Note: "+noten[y].getWert() + "\nGewichtung: " + noten[y].getGewichtung() + "\nDatum: " + noten[y].getGewichtung());
-//            }
-//            System.out.println("------------------------------------------------------------------");
-//        }
-
-
-
-
+        System.out.println("Nachname: " + s2.getName() + "\nVorname: " + s2.getVorname() + "\nGeburtstag: " + s2.getGeburtstag());
+        s2.printFaecher();
+        System.out.println("------------------------------------------------------------------");
     }
 
 }

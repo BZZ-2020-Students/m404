9316e36283a1d3a384cc65ee5ee368a44e05fa91
Muetzilla <muetz-gitkraken@gasbichler.ch>
2021-05-25 10:38:01 +0200

    added attributes

--- muetzilla_stuff/src/notenverwaltung/Fach.java
+++ muetzilla_stuff/src/notenverwaltung/Fach.java
@@ -1,4 +1,48 @@
 package notenverwaltung;
 
 public class Fach {
+    private Note[] noten;
+    private String name;
+    private int semester;
+    private int schuljahr;
+
+    public Fach(Note[] noten, String name, int semester, int schuljahr) {
+        this.noten = noten;
+        this.name = name;
+        this.semester = semester;
+        this.schuljahr = schuljahr;
+    }
+
+    public Note[] getNoten() {
+        return noten;
+    }
+
+    public void setNoten(Note[] noten) {
+        this.noten = noten;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public int getSchuljahr() {
+        return schuljahr;
+    }
+
+    public void setSchuljahr(int schuljahr) {
+        this.schuljahr = schuljahr;
+    }
 }
+
--- muetzilla_stuff/src/notenverwaltung/Schule.java
+++ muetzilla_stuff/src/notenverwaltung/Schule.java
@@ -17,5 +17,6 @@ public class Schule {
     }
 
     public void run() {
+        Schueler s1 = new Schueler();
     }
 }

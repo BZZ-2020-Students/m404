b14d21cc0c3df1d49463303926696d2517ed5ec7
Muetzilla <muetz-gitkraken@gasbichler.ch>
2021-05-25 10:14:28 +0200

    some changes

--- muetzilla_stuff/src/scanner/Scan.java
+++ muetzilla_stuff/src/scanner/Scan.java
@@ -11,10 +11,6 @@ public class Scan {
 
     public int scanInt() {
         int input = scanner.nextInt();
-        while (scanner.hasNextInt()) {
-            System.out.print("Bitte geben Sie einen gültigen Wert ein > ");
-            input = scanner.nextInt();
-        }
         return input;
     }
 
@@ -31,14 +27,11 @@ public class Scan {
 
     public char scannAnyChar() {
         char input = scanner.next().charAt(0);
-        //Cache leeren
-        scanner.nextLine();
         return input;
     }
 
     public char scannJustLetter() {
         char input = scanner.next().charAt(0);
-
         while (!Character.isLetter(input)) {
             System.out.print("Bitte einen Buchstaben eingeben >  ");
             input = scanner.next().charAt(0);
@@ -49,20 +42,12 @@ public class Scan {
 
     public double scannNextDouble() {
         double input = scanner.nextDouble();
-        /*while (scanner.hasNextDouble()) {
-            System.out.print("Bitte geben Sie einen gültigen Wert ein > ");
-            input = scanner.nextDouble();
-        }
-        System.out.println("BAKSJDaisdjfjdflkajdlfköjaslköfdj");*/
         return input;
     }
 
     public float scannNextFloat() {
         float input = scanner.nextFloat();
-        while (scanner.hasNextFloat()) {
-            System.out.print("Bitte geben Sie einen gültigen Wert ein > ");
-            input = scanner.nextFloat();
-        }
+
         return input;
     }
 }

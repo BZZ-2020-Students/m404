e58c99527059d154baa7591f891ea9b865c5cf00
Muetzilla <muetz-github@gasbichler.ch>
2021-06-01 16:23:04 +0200

    Add files via upload

--- muetzilla_stuff/src/uri/URI.java
+++ muetzilla_stuff/src/uri/URI.java
@@ -1,4 +1,27 @@
 package uri;
 
+import pythagoras.Calculation;
+import scanner.Scan;
+
+/**
+ * short description of this program
+ *
+ * @author
+ * @version 1.0
+ * @date 2021.06.01
+ */
 public class URI {
+    Scan scanner;
+    Calculation calc;
+
+    public URI() {
+        scanner = new Scan();
+        calc = new Calculation();
+    }
+
+    /**
+     * starts the execution
+     *
+     * @param args command line arguments
+     */
 }

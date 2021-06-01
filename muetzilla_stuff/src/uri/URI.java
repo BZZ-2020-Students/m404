c2079b925573708a3a16fa3273c30ef1f26ac454
Muetzilla <muetz-github@gasbichler.ch>
2021-06-01 16:36:32 +0200

    HONK

--- muetzilla_stuff/src/uri/URI.java
+++ muetzilla_stuff/src/uri/URI.java
@@ -24,4 +24,44 @@ public class URI {
      *
      * @param args command line arguments
      */
+
+    public static void main(String[] args) {
+        URI uri = new URI();
+        uri.run();
+    }
+
+    public void run() {
+        boolean nextCalculation;
+        String u;
+        String r;
+        String i;
+        double result = 0;
+
+        do {
+            System.out.print("Willkommen beim URI Rechner\n###############################################\nGeben Sie zwei der 3 Grössen an. In der letzen geben Sie ein ? ein.\n");
+            System.out.print("U > ");
+            u = scanner.scanString();
+            System.out.print("R > ");
+            r = scanner.scanString();
+            System.out.print("I > ");
+            i = scanner.scanString();
+
+            if (u.equals("?")) {
+                result = calc.seiteA(r, i);
+                System.out.println("U beträgt: " + result);
+            } else if (r.equals("?")) {
+                result = calc.seiteB(u, i);
+                System.out.println("R beträgt: " + result);
+            } else if (i.equals("?")) {
+                result = calc.seiteC(u, r);
+                System.out.println("I beträgt: " + result);
+            } else {
+                System.out.println("Kein ? vorhanden. Bitte versuchen Sie es erneut!");
+            }
+
+            System.out.print("Möchten Sie noch eine Berechnung machen (true/false) > ");
+            nextCalculation = scanner.scanBoolean();
+
+        } while (nextCalculation);
+    }
 }

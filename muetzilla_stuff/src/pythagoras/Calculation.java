36e5af372b40d9e7387f0d7a9656af598357148c
Muetzilla <muetz-github@gasbichler.ch>
2021-06-01 16:00:03 +0200

    changes Numero 12

--- muetzilla_stuff/src/pythagoras/Calculation.java
+++ muetzilla_stuff/src/pythagoras/Calculation.java
@@ -5,4 +5,12 @@ public class Calculation {
     public double seiteA(String seiteB, String seiteC) {
         return 0;
     }
+
+    public double seiteB(String seiteA, String seiteC) {
+        return 0;
+    }
+
+    public double seiteC(String seiteA, String seiteB) {
+        return 0;
+    }
 }

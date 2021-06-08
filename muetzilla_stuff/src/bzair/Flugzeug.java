6a9f47608aad1cfa247a569ed825d7e9298bbc92
Muetzilla <muetz-github@gasbichler.ch>
2021-06-08 14:10:11 +0200

    Ohhh nein! Corona auf Brumm Brumm

--- muetzilla_stuff/src/bzair/Flugzeug.java
+++ muetzilla_stuff/src/bzair/Flugzeug.java
@@ -1,2 +1,16 @@
-package bzair;public class Flugzeug {
+package bzair;
+
+import scanner.Scan;
+
+public class Flugzeug {
+    Scan scanner;
+
+    public static void main(String[] args) {
+        Flugzeug flugzeug = new Flugzeug();
+        flugzeug.run();
+    }
+
+    public void run() {
+    }
+
 }
--- muetzilla_stuff/src/bzair/Passagier.java
+++ muetzilla_stuff/src/bzair/Passagier.java
@@ -1,2 +1,62 @@
-package bzair;public class Passagier {
+package bzair;
+
+import java.time.LocalDate;
+
+public class Passagier {
+    private int sitznummer;
+    private String vorname;
+    private String nachname;
+    private LocalDate gebDatum;
+    private boolean positivesTestergebnis;
+
+    public Passagier() {
+    }
+
+    public Passagier(int sitznummer, String vorname, String nachname, LocalDate gebDatum, boolean positivesTestergebnis) {
+        this.sitznummer = sitznummer;
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.gebDatum = gebDatum;
+        this.positivesTestergebnis = positivesTestergebnis;
+    }
+
+    public int getSitznummer() {
+        return sitznummer;
+    }
+
+    public void setSitznummer(int sitznummer) {
+        this.sitznummer = sitznummer;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public LocalDate getGebDatum() {
+        return gebDatum;
+    }
+
+    public void setGebDatum(LocalDate gebDatum) {
+        this.gebDatum = gebDatum;
+    }
+
+    public boolean isPositivesTestergebnis() {
+        return positivesTestergebnis;
+    }
+
+    public void setPositivesTestergebnis(boolean positivesTestergebnis) {
+        this.positivesTestergebnis = positivesTestergebnis;
+    }
 }

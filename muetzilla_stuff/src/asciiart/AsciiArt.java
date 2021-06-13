b76547d24f8db383bbfe0c59f1ad4ce35c188452
Muetzilla <muetz-github@gasbichler.ch>
2021-06-13 18:15:52 +0200

    DOING WORK ASCII ART

--- muetzilla_stuff/src/asciiart/AsciiArt.java
+++ muetzilla_stuff/src/asciiart/AsciiArt.java
@@ -1,24 +1,36 @@
+package asciiart;
+
+import scanner.Scan;
+
+import java.util.Locale;
+
 /**
  * Template Klasse für simple Java-Programme
  *
  * @author Ibi dumm
- * @since 2021.06.08
  * @version 0.1
+ * @since 2021.06.08
  */
 public class AsciiArt {
 
     public static void main(String[] args) {
-        AsciiArt programm = new AsciiArt();
-        programm.run();
+        AsciiArt asc = new AsciiArt();
+        asc.run();
     }
 
     private void run() {
-        // TODO: Implement logic
-        for (int j = 0; j < 24; j++) {
-            for (int i = 0; i < 6; i++) {
-                System.out.println(ArtText.letters[j][i]);
+        Scan scanner = new Scan();
+        System.out.print("Was möchten Sie in Ascii Art dargestellt haben (Eingabe in Kleinbuchstaben und ohne Sonderzeichen sowie Leerzeichen) > ");
+        String input = scanner.scanString();
+        input.toLowerCase(Locale.ROOT);
+        for (int i = 0; i < 6; i++) {
+            for (char c : input.toCharArray()) {
+                int charInInt = c;
+                System.out.print(ArtText.letters[charInInt - 97][i]);
             }
+            System.out.println();
         }
-
     }
+
+
 }

23e65ff72cf020f0369f53efc848cabe07efaf6d
Alyssa <heimlichera@bzz.ch>
2021-06-15 10:07:19 +0200

    :v

---
+++ ash_stuff/m404/src/airline/AirlineDemo.java
@@ -0,0 +1,15 @@
+package airline;
+
+import scanner.Einleser;
+
+public class AirlineDemo {
+    public static void main(String[] args) {
+        new AirlineDemo().run();
+    }
+    public void run() {
+        Einleser e  = new Einleser();
+
+
+
+    }
+}

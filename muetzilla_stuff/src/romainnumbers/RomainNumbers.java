cb0eeb7712ff363d0db339d028d72d2f96c95cd8
Muetzilla <muetz-github@gasbichler.ch>
2021-06-15 16:09:06 +0200

    USE STRING BUILDER FOR THE FIRST TIME BUT NOOO ENUM BRUMMMMMM

---
+++ muetzilla_stuff/src/romainnumbers/RomainNumbers.java
@@ -0,0 +1,37 @@
+package romainnumbers;
+
+
+import scanner.Scan;
+
+public class RomainNumbers {
+    Scan scanner;
+    String[] romainNumbers = {"I", "V", "X", "L", "C", "D", "M", "ↁ", "ↁ", "ↇ", "ↈ"};
+
+    public RomainNumbers() {
+        scanner = new Scan();
+    }
+
+    public static void main(String[] args) {
+        RomainNumbers roNu = new RomainNumbers();
+        roNu.run();
+    }
+
+    public void run() {
+        int arabicNumberInput;
+        do {
+            System.out.print("Geben Sie einge ganze Zahl zwischen 1 und 9999 ein > ");
+            arabicNumberInput = scanner.scanInt();
+        } while (arabicNumberInput > 9999 || arabicNumberInput < 1);
+        System.out.println("Die Zahl " + arabicNumberInput + " entspricht der römischen Zahl " + convertToRomanNumber(arabicNumberInput));
+    }
+
+    private String convertToRomanNumber(int arabicNumberInput) {
+        StringBuilder romanNumber = new StringBuilder();
+        for (int i = 0; i < arabicNumberInput; i++) {
+            romanNumber.append("I");
+        }
+        return romanNumber.toString();
+    }
+
+
+}

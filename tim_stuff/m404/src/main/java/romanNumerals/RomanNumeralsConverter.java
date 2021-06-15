0961b68e2c5ff5522455ef4c9fc84d6ba343d04c
Tim Irmler <zwazel@hotmail.com>
2021-06-15 10:08:34 +0200

    smallo improvemento

--- tim_stuff/m404/src/main/java/romanNumerals/RomanNumeralsConverter.java
+++ tim_stuff/m404/src/main/java/romanNumerals/RomanNumeralsConverter.java
@@ -10,27 +10,27 @@ public class RomanNumeralsConverter {
     private void run() {
         System.out.print("Bitte ihre Zahl eingeben > ");
         int number = scanner.readInt();
-        while(number > 9999 || number < 1) {
+        while (number > 9999 || number < 1) {
             System.out.println("Die Zahl muss zwischen 1 und 9999 liegen!");
             System.out.print("Bitte ihre Zahl eingeben > ");
             number = scanner.readInt();
         }
 
-        RomanNumerals currentRoman = RomanNumerals.M;
+        int startNumber = number;
         StringBuilder romanNumberString = new StringBuilder();
-        while(number > 0) {
-            for(RomanNumerals romanNumeral : RomanNumerals.values()) {
-                if(number >= romanNumeral.getValue()) {
-                    System.out.println("Current Numeral: " + romanNumeral + "\nnumber before: " + number);
+        while (number > 0) {
+            for (RomanNumerals romanNumeral : RomanNumerals.values()) {
+                if (number >= romanNumeral.getValue()) {
+//                    System.out.println("Current Numeral: " + romanNumeral + "\nnumber before: " + number);
                     number -= romanNumeral.getValue();
-                    System.out.println("Number after: " + number);
+//                    System.out.println("Number after: " + number);
                     romanNumberString.append(romanNumeral);
                     break;
                 }
             }
         }
 
-        System.out.println(romanNumberString);
+        System.out.println(startNumber + " = " + romanNumberString);
     }
 
     public static void main(String[] args) {

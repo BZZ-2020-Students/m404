8a0706cf199ded32fd38fb083f1461f6efb77afc
Alyssa <heimlichera@bzz.ch>
2021-06-15 10:49:13 +0200

    roman numerals

--- ash_stuff/m404/src/numerals/NumeralsDemo.java
+++ ash_stuff/m404/src/numerals/NumeralsDemo.java
@@ -1,13 +1,38 @@
 package numerals;
 
 
+import java.util.Scanner;
+
 public class NumeralsDemo {
     public static void main(String[] args) {
         new NumeralsDemo().run();
     }
 
     public void run(){
-        numEinleser e = new numEinleser();
-        e.readString();
+        Scanner sc= new Scanner(System.in);
+
+        String numerals = "";
+        String[] symbols = {"M", "D", "C", "L", "X", "V", "I"};
+        int[] numbers = {1000, 500, 100, 50, 10, 5, 1};
+        int input;
+        int nr = 0;
+        int index = 0;
+
+
+        System.out.print("Enter a number >");
+        input = sc.nextInt();
+
+        nr += input;
+        do{
+            if(nr >= numbers[index]){
+                numerals += symbols[index];
+                nr = nr - numbers[index];
+            }else{
+                index++;
+            }
+        }while(nr > 0);
+
+        System.out.println("Roman numerals to "+ input + " are: " + numerals);
     }
+
 }
